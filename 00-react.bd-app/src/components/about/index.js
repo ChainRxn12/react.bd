b9a7a3997f52c2bac1e2681eabb88b74a2205f0d
Billy Dorman <b.d.dorman12@gmail.com>
2021-09-14 13:19:01 -0500

    create components folder, add about and contact info code

---
+++ 00-react.bd-app/src/components/about/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+
+function About() {
+  return (
+    <section class="about">
+      <h1 id="about">About Billy Dorman</h1>
+      <p>
+          I am a Web Developer with a keen eye for creating visually appealing and 
+          user friendly applications. During Bootcamp, me and two team members developed a 
+          Cryptocurrency News Application using two API resources and the lesser utilized 
+          Bulma CSS Framework. After receiving a Certificate in Full-Stack Web Development 
+          from UTSA Bootcamp, I’ve enjoyed working on the Front-End of applications--
+          specifically with CSS and bringing the page to life with JavaScript functionality. 
+          In the future, I see myself as a Full-Stack Developer leading a team that creates a 
+          variety of web applications. I believe my background as a supervisor in the oilfield 
+          has prepared me for working with teams and exceeding client demand and expectations.
+     </p>
+    </section>
+  );
+}
+
+export default About;
